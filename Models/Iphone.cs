ac40248e82008535f64b98039e42f550cf8a94ad
Gberga <gbergadev@gmail.com>
2024-01-10 21:57:59 -0300

    primeira versão

---
+++ Models/Iphone.cs
@@ -0,0 +1,11 @@
+namespace DesafioPOO.Models
+{
+    // TODO: Herdar da classe "Smartphone"
+    public class Iphone: Smartphone
+    {
+        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(){
+
+        }
+    }
+}
